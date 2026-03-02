30b4e51d41e1b9b59c3fdbc4e2a6b0585fd5b198
TaushifReza <taushif1teza@gmail.com>
2026-03-02 17:22:11 +0545

    feat: configure db connection with connectionn pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/TaushifReza/go-social/internal/db"
 	"github.com/TaushifReza/go-social/internal/env"
 	"github.com/TaushifReza/go-social/internal/store"
 	"github.com/joho/godotenv"
@@ -15,9 +16,26 @@ func main() {
 
 	config := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://postgres:root786@172.21.64.1/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		config.db.addr,
+		config.db.maxOpenConns,
+		config.db.maxIdleConns,
+		config.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic("database connection error: ", err)
+	}
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: config,
