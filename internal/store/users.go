2bdfec148d6d1c2d00b94841c0d49dc6d5ed7c82
TaushifReza <taushif1teza@gmail.com>
2026-03-02 15:37:46 +0545

    feat: create model

---
+++ internal/store/users.go
@@ -0,0 +1,36 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/TaushifReza/go-social/internal/model"
+)
+
+type UserStore struct {
+	db *sql.DB
+}
+
+func (s *UserStore) Create(ctx context.Context, user *model.User) error {
+	query := `
+    INSERT INTO users (username, password, email)
+    VALUES (1$, 2$, 3$) RETURNING id, created_at
+    `
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		user.UserName,
+		user.Password,
+		user.Email,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
