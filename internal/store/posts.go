7cb5f8fb63e0797af6e1c1724419489f350d3df2
TaushifReza <taushif1teza@gmail.com>
2026-05-06 22:22:25 +0545

    feat: add filter in user feed api

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -124,16 +124,21 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedF
         LEFT JOIN comments c ON c.post_id = p.id
         LEFT JOIN users u ON p.user_id = u.id
         JOIN followers f ON f.follower_id = p.user_id OR p.user_id = $1
-    WHERE f.user_id = $1 AND p.user_id = $1
-    GROUP BY p.id, p.created_at, u.id, u.username
-    ORDER BY p.created_at %s
+    WHERE
+        f.user_id = $1 AND
+        (p.title ILIKE '%%' || $4 || '%%' OR p.content ILIKE '%%' || $4 || '%%')
+        (p.tags @> $5 OR $5 = '{}')
+    GROUP BY
+        p.id, p.created_at, u.id, u.username
+    ORDER BY
+        p.created_at %s
     LIMIT $2 OFFSET $3
     `, fq.Sort)
 
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
 	defer cancel()
 
-	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset)
+	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset, fq.Search, pq.Array(fq.Tags))
 
 	if err != nil {
 		return nil, err
